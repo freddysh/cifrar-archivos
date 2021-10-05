0d437e2bb98697ae250d045e12a6690b7792cb3c
Freddy Silva <fsilva@LINUXGDP042.alignet.com>
2021-10-05 12:16:12 -0500

    coreccion de codigo smells

--- handler.py
+++ handler.py
@@ -22,21 +22,20 @@ def descargar(event,cotext):
 
 # Funciones para encriptar
 def encriptar_transferir(key,origin_disc):
-    ubicacionMensaje="error.handler.encriptar_transferir"
-    errorMensaje="error"
+    ubicacion_mensaje="error.handler.encriptar_transferir"
+    error_mensaje="error"
     # iniciamos la funcion para conectar al bucket s3
     s3 = boto3.client('s3', endpoint_url=ENDPOINT_URL,
             aws_access_key_id = AWS_ACCESS_KEY_ID,
             aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
             region_name=REGION_NAME)
     try:
-        bucket=BUCKET
-        s3.head_bucket(Bucket=bucket)
+        s3.head_bucket(Bucket=BUCKET)
         if not isinstance(boto3.client('s3'), botocore.client.BaseClient):
-            return {"status": errorMensaje, "message":ubicacionMensaje, "messageDetail":"Error en la conexion"}
+            return {"status": error_mensaje, "message":ubicacion_mensaje, "messageDetail":"Error en la conexion"}
         
         if not os.path.isdir(origin_disc):
-            return {"status": errorMensaje, "message":ubicacionMensaje, "messageDetail":"La carpeta de origen ("+origin_disc+") no existe."}
+            return {"status": error_mensaje, "message":ubicacion_mensaje, "messageDetail":"La carpeta de origen ("+origin_disc+") no existe."}
         
         else:
             # generamos el hash con la clave
@@ -46,7 +45,7 @@ def encriptar_transferir(key,origin_disc):
             # obtenemos los archivos de la carpeta de origen y recorremos para subir al bucket s3
             contenidos=os.listdir(origin_disc)
             if len(contenidos)==0:
-                return {"status": errorMensaje, "message":ubicacionMensaje, "messageDetail":"No se encontraron archivos para subir en el origen({})".format(origin_disc)}
+                return {"status": error_mensaje, "message":ubicacion_mensaje, "messageDetail":"No se encontraron archivos para subir en el origen({})".format(origin_disc)}
                 
             lista_archivos_no_subidos=[]
             for elemento in contenidos:
@@ -55,8 +54,8 @@ def encriptar_transferir(key,origin_disc):
                     # print(encripted_archive_url)
                     if type(encripted_archive_url).__name__=='str':
                     # subimos los archivos al bucker s3
-                        s3.upload_file(origin_disc+"/"+encripted_archive_url, bucket, encripted_archive_url)
-                        results = s3.list_objects(Bucket=bucket, Prefix=encripted_archive_url)
+                        s3.upload_file(origin_disc+"/"+encripted_archive_url, BUCKET, encripted_archive_url)
+                        results = s3.list_objects(Bucket=BUCKET, Prefix=encripted_archive_url)
                         if 'Contents' not in results:
                             lista_archivos_no_subidos.append({"archivo":elemento,"motivo":"No se subio el archivo"})
                         os.remove(origin_disc+"/"+encripted_archive_url)
@@ -70,33 +69,33 @@ def encriptar_transferir(key,origin_disc):
     
     except  botocore.exceptions.ConnectionError as ex:
         # print(ex)
-        return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"Error de conexion"}
+        return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"Error de conexion"}
     
     except  botocore.exceptions.NoCredentialsError as ex:
         # print(ex)
-        return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"Error de credenciales"}
+        return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"Error de credenciales"}
     
     except  s3.exceptions.NoSuchBucket as ex:
-        return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"El bucket({}) no existe".format(BUCKET)}
+        return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"El bucket({}) no existe".format(BUCKET)}
     
     except  s3.exceptions.NoSuchKey as ex:
-        return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"No se encontro el archivo en el bucket({})".format(BUCKET)}
+        return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"No se encontro el archivo en el bucket({})".format(BUCKET)}
     
     except botocore.exceptions.ClientError as e:
         error_code = int(e.response['Error']['Code'])
         if error_code == 403:
-            return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"El bucket({}) esta en modo privado".format(BUCKET)}
+            return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"El bucket({}) esta en modo privado".format(BUCKET)}
     
         elif error_code == 404:
-            return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"El bucket({}) no existe".format(BUCKET)}
+            return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"El bucket({}) no existe".format(BUCKET)}
     
     except Exception as ex:
         print(ex.args)
-        return {"status": errorMensaje, "message": "error.handler.transferir_local_boto3", "messageDetail":ex.args}
+        return {"status": error_mensaje, "message": "error.handler.transferir_local_boto3", "messageDetail":ex.args}
 
 def decriptar_descargar(key1,destino):
-    ubicacionMensaje="error.handler.decriptar_descargar"
-    errorMensaje="error"
+    ubicacion_mensaje="error.handler.decriptar_descargar"
+    error_mensaje="error"
     s3 = boto3.client('s3', endpoint_url=ENDPOINT_URL,
             aws_access_key_id = AWS_ACCESS_KEY_ID,
             aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
@@ -107,22 +106,21 @@ def decriptar_descargar(key1,destino):
         if type(clave).__name__ !='bytes':
             return clave
 
-        bucket=BUCKET
-        s3.head_bucket(Bucket=bucket)
+        s3.head_bucket(Bucket=BUCKET)
         if not isinstance(boto3.client('s3'), botocore.client.BaseClient):
-            return {"status": errorMensaje, "message":ubicacionMensaje, "messageDetail":"Error en la conexion"}
+            return {"status": error_mensaje, "message":ubicacion_mensaje, "messageDetail":"Error en la conexion"}
             
         if not os.path.isdir(destino):
-            return {"status": errorMensaje, "message":ubicacionMensaje, "messageDetail":"La carpeta de destino ("+destino+") no existe."}
+            return {"status": error_mensaje, "message":ubicacion_mensaje, "messageDetail":"La carpeta de destino ("+destino+") no existe."}
     
         else:
-            list=s3.list_objects(Bucket=bucket)['Contents']
+            list=s3.list_objects(Bucket=BUCKET)['Contents']
             if len(list)==0:
-                return {"status": errorMensaje, "message":ubicacionMensaje, "messageDetail":"No se encontraron archivos para descargar del bucket({})".format(bucket)}
+                return {"status": error_mensaje, "message":ubicacion_mensaje, "messageDetail":"No se encontraron archivos para descargar del bucket({})".format(BUCKET)}
            
             lista_archivos_erroneos=[]
             for key in list:
-                    s3.download_file(bucket, key['Key'], destino+key['Key'])
+                    s3.download_file(BUCKET, key['Key'], destino+key['Key'])
                     if os.path.isfile(destino+key['Key']):
                         rpt=desencriptar_archivoAESDome(destino+key['Key'],clave)
                         if rpt is not True:
@@ -134,29 +132,29 @@ def decriptar_descargar(key1,destino):
     
     except  botocore.exceptions.ConnectionError as ex:
         print(ex)
-        return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"Error de conexion"}
+        return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"Error de conexion"}
 
     except  botocore.exceptions.NoCredentialsError as ex:
         print(ex)
-        return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"Error de credenciales"}
+        return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"Error de credenciales"}
     
     except  s3.exceptions.NoSuchBucket as ex:
-        return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"El bucket({}) no existe".format(BUCKET)}
+        return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"El bucket({}) no existe".format(BUCKET)}
     
     except  s3.exceptions.NoSuchKey as ex:
-        return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"No se encontro el archivo en el bucket({})".format(BUCKET)}
+        return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"No se encontro el archivo en el bucket({})".format(BUCKET)}
     
     except botocore.exceptions.ClientError as e:
         error_code = int(e.response['Error']['Code'])
         if error_code == 403:
-            return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"El bucket({}) esta en modo privado".format(BUCKET)}
+            return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"El bucket({}) esta en modo privado".format(BUCKET)}
     
         elif error_code == 404:
-            return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":"El bucket({}) no existe".format(BUCKET)}
+            return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":"El bucket({}) no existe".format(BUCKET)}
     
     except Exception as ex:
         # return ex
-        return {"status": errorMensaje, "message": ubicacionMensaje, "messageDetail":ex.args}
+        return {"status": error_mensaje, "message": ubicacion_mensaje, "messageDetail":ex.args}
 
 def generar_claveDome(key):
     try: 
